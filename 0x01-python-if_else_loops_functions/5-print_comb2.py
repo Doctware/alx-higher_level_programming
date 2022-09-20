4039451be180e0a48111d75dc9522edbdaddc3f6
Doctware <yusclever9@gmail.com>
2022-09-20 16:48:38 -0700

    Print combination 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 99):
-    print("{}".format(i))
+for i in range(100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{},".format(str(i).zfill(2)), end=" ")
