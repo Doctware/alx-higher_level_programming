f70f9d674b067c2200f001b7cc76d5b642badcb1
Doctware <yusclever9@gmail.com>
2022-09-22 14:17:22 -0700

    The discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    # Print sorted name fom dirctory
-    for name in sorted(dir(hidden_4)):
-        # Print only name that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
+    lines = dir(hidden_4)
+    for i in lines:
+        if i[:2] != "__":
+            print(i)
