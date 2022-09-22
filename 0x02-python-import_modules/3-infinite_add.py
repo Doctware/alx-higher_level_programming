ebfb29c99f67b8ece6190d5eff476fe146decb89
Doctware <yusclever9@gmail.com>
2022-09-22 14:28:37 -0700

    The infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    arg = sys.argv
-
-    sum = 0
-    if len(arg) > 1:
-        for i in arg:
-            if i != arg[0]:
-                sum += int(i)
-                
-    print("{}".format(sum))
+    args = sys.argv
+    argc = len(args) - 1
+    total = 0
+    if argc == 0:
+        print(total)
+    else:
+        for i in range(0, argc):
+            total = total + int(args[i + 1])
+        print(total)
