5749111490c4d51ea2021b70d01c88af13735b18
Doctware <yusclever9@gmail.com>
2023-11-01 00:44:58 +0100

    Fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("buzz", end=" ")
         else:
             print(i, end=" ")
