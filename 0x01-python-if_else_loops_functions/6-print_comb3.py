ee845051fae6a8c3b6b5b512b6359a78fb955e07
Doctware <yusclever9@gmail.com>
2023-10-31 18:05:13 +0100

    we want diffrent combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for tens_d in range(10):
     for one_d in range(tens_d + 1, 10):
         if tens_d != one_d:
             if tens_d == 8 and one_d == 9:
-                print(f"{tens_d}{one_d}")
+                print("{}{}".format(tens_d, one_d))
             else:
-                print(f"{tens_d}{one_d}, ", end='')
+                print("{}{}, ".format(tens_d, one_d), end='')
