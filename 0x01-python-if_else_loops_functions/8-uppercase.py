2f2498622b248072eb6a57f7a112d9359950c0c1
Doctware <yusclever9@gmail.com>
2023-11-01 00:00:30 +0100

    We are changing to upper no more lower

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for ch in str:
         if 97 <= ord(ch) <= 122:
             ch = chr(ord(ch) -32)
-        print("{}".format(ch) end='')
+        print("{}".format(ch), end='')
     print()
