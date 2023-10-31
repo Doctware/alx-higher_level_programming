5db9b5955b0be7bac3d65c35eec1ee910a2ef08d
Doctware <yusclever9@gmail.com>
2023-10-31 12:41:56 +0100

    lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - checkes if a singly liked list have a cycle
+ * @list: [ointer to the head of th e linked list
+ * Return: 1 if ture,  else 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
