03d0ed3b64de0cf0e374aa0beba545b0fea68eab
Doctware <yusclever9@gmail.com>
2023-11-01 01:16:47 +0100

    Lower & Upper mixer

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,7 @@
 for ch in range(ord('Z'), ord('A') - 1, -1):
     char = chr(ch)
     if ch % 2 == 0:
-        char = char.upper()
-    else:
         char = char.lower()
+    else:
+        char = char.upper()
     print("{}".format(char), end="")
-print()
