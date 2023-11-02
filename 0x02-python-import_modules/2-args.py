d6a7e2a789ab0fa27b27eec452cc92b78e00481a
Doctware <yusclever9@gmail.com>
2023-11-02 16:52:24 +0100

    The argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if num_arg == 0:
         print("0 arguments.")
     elif num_arg == 1:
-        print("1: argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("1 argument:")
+        print("1 {}:".format(sys.argv[1]))
     else:
-        print("{}: arguments:".format(num_arg))
+        print("{} arguments:".format(num_arg))
         for i in range(1, num_arg + 1):
             print("{}: {}".format(i, sys.argv[i]))
