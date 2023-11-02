8ed24be03c159421366ffeaf43b289a17ffd1949
Doctware <yusclever9@gmail.com>
2023-11-02 14:30:18 +0100

    The adder

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """ print sum of 1 abd 2"""
-    add = __import__('add_0').add
+    from add_0 import add
 
     a = 1
     b = 2
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
