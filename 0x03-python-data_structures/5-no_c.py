a84f73c6b9581a91f18acc6899d6b3c59966374f
Doctware <yusclever9@gmail.com>
2023-11-06 16:00:27 +0100

    No more c :|

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+
+    """ remove letter C & c """
+
+    new_string = ""
+
+    for char in my_string:
+        if char != 'C' and char != 'c':
+            new_string += char
+        
+    return new_string
