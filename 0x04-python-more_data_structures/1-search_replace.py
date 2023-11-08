cdd375d7a7ac2b005c545eb84be7954ab6b669d0
Doctware <yusclever9@gmail.com>
2023-11-08 13:26:52 +0100

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+
+    """ Search and replace """
+
+    new_list = [
+        x
+        if x != search
+        else replace
+        for x in my_list
+    ]
+
+    return new_list
