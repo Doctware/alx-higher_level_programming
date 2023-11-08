3c8eee003c8968015a0d14d0655171fba1d18878
Doctware <yusclever9@gmail.com>
2023-11-08 16:54:11 +0100

    Get the common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,10 +2,7 @@
 def common_elements(set_1, set_2):
 
     """ Return the common element bitween two set """
+    
+    result = set_1.intersection(set_2)
 
-    for look_1 in set_1:
-        for look_2 in set_2:
-            if look_1 == look_2:
-                result = look_1
-
-        return result
+    return result
