a199c9a62fd801ba586e1e6f6af143b29b47cc34
Doctware <yusclever9@gmail.com>
2023-11-08 15:44:29 +0100

    Add unique integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+
+    """ Adds all uniq integers in a list """
+    """ Only oce for all integer """
+
+    sum_int = sum(my_list)
+
+    return sum_int
