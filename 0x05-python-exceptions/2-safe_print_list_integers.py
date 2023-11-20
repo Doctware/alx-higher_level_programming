99250182c55a031e8770bf592119323b2fb951a8
Doctware <yusclever9@gmail.com>
2023-11-20 21:46:15 +0100

    Ssfely print integer list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    
     """ print the first x element of a list and all integers """
-    try:
+
+    for i in range(x):
         count = 0
 
-        for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
-            count += 1
+            count += i
 
-    except (ValueError, TypeError):
-        pass
+        except (ValueError, TypeError):
+            pass
 
-    except IndexError:
-        print("list index out of range")
-
+    print()
     return count
