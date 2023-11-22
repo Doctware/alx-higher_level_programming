7d3609362a3d14c0a0daefde3d1087947a8e397c
Doctware <yusclever9@gmail.com>
2023-11-22 11:54:37 +0100

    Do not access the private class instnce

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A class square that defines a square by:
+    (based on 0-square.py) """
+
+
+class Square:
+    """ Define an initialization method """
+
+    def __init__(self, size):
+        """
+        Inisialized the new instance of the square class
+
+        Paramiters:
+        size (int): the size of the square
+
+        """
+        self.__size = size
+
