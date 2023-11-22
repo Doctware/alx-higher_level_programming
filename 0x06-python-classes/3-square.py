ffae6f7894f5db58b7541b1162300dfb476def9c
Doctware <yusclever9@gmail.com>
2023-11-22 15:24:43 +0100

    Check the Docs to learn more

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" A class square that defines a square by:
+    (base on 2-square.py) """
+
+
+class Square:
+    """ define init method """
+
+    def __init__(self, size):
+        """
+        Inisialized size as the size of the square
+
+        parameters:
+
+        size (int): the size o the square
+
+        Verify size if it is int else raise TypeError
+
+        Check size if it is < 0 else raise ValueError
+
+        """
+
+        try:
+            self.__size = size
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+        finally:
+            pass
+
+    """
+    Defining a public instance method thas returns
+    the current ssquare area
+    """
+
+    def area(sellf):
+
+        """
+        computing area of square
+
+        Returning area of square
+        """
+
+        return self.__size ** 2
