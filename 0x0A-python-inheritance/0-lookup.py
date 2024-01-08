f55fe5ceee54bcb578bbd66df824f04e7d250b35
Doctware <yusclever9@gmail.com>
2024-01-08 17:54:28 +0100

    Looking up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" this function returns the list of availble attributes
+def lookup(obj):
+    """ this function returns the list of availble attributes
     and method of an object """
 
-
-def lookup(obj):
     return dir(obj)
