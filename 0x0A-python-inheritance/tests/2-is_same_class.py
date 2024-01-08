677366966cda422738603280a49b4d172598ffdf
Doctware <yusclever9@gmail.com>
2024-01-09 00:28:21 +0100

    Knowing the valid instance worth it

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" A function thats return True if the object
+    is an insance of the specific class; else False """
+
+
+def is_same_class(obj, a_class):
+    """
+    return True if obj is exacly an insatanse
+    of the provided class
+
+    else return False
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
