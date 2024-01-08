17408b74f7d68734bc7de05ce816e00945598d1a
Doctware <yusclever9@gmail.com>
2024-01-09 00:35:57 +0100

    Knowing the valid instance worth it

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,8 +11,4 @@ def is_same_class(obj, a_class):
 
     else return False
     """
-
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
