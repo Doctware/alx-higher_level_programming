6a91a3cd6daa211016c6dcb7cff57afcaf8130bd
Doctware <yusclever9@gmail.com>
2024-01-09 01:54:43 +0100

    The Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A class BaseGeometry (based on 5-base_geometry) """
+
+
+class BaseGeometry:
+    """ A class base on 5-base_geometry """
+
+    def area(self):
+        """
+        public method area that raises an exception
+        """
+        raise Exception("area() is not implemented")
