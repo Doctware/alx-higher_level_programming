2a13cc5c9ee909d8d038825b6b411f562e281208
Doctware <yusclever9@gmail.com>
2024-02-01 01:37:27 +0100

    work in progress

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" A class Rectangle that defines rectangle
+    by (by base on 7-rectangle.py)"""
+
+
+class Rectangle:
+    """
+    defining public class attribute
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        inisializing init method parameter
+
+        width and height
+
+        public class attribute number_of_instances
+        will be incresing during each new instance 
+        """
+        self.width = width
+        self.height height
+        Rectangle.number_of_instances += 1
+
+
+    @property
+    def width(self):
+        """
+        getting the width
+
+        Returnig it
+        """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """
+        setting width
+
+        check if width is int, if no
+        raise TypeError with geiven message
+        """
+
+        try:
+            if not isinstance(value, int):
+                raise TypeError("width must be am ineget")
+            elif value  < 0:
+                raise ValueError("width must >= 0")
+            else:
+                self.__width = value
+
+
