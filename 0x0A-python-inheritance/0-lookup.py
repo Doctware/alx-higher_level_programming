5ed835c22b91bb44a8dd3c0ccd33917c6a87fd08
Doctware <yusclever9@gmail.com>
2024-02-05 16:20:28 +0100

    Let look up for better days

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining a function thats returns the list
+    of available attributes and methos of an object """
+
+
+def lookup(obj):
+    """
+    return the list of attributes and and methods of an object
+    """
+    return obj.__dict__
