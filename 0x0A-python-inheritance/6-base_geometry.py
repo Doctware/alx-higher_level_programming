b3f2d0c0d1197474fafd36304d3b9d6af024f743
Doctware <yusclever9@gmail.com>
2024-02-06 02:07:28 +0100

    6-main.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A class BaseGeometry (base on 5-base_goemetry) """
+
+
+class BaseGeometry:
+    """ definin public method area """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
