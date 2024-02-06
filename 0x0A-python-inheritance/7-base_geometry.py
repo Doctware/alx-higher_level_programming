d2c49a49af1534d74b3938a0b14d719f12334eac
Doctware <yusclever9@gmail.com>
2024-02-06 02:34:26 +0100

    7-main.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A class BaseGeometry (base on 5-base_goemetry) """
+
+
+class BaseGeometry:
+    """ definin public method area """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        check if value is not an integer
+        raise TypeError with the given exception message
+
+        If value is <= 0: raie ValueError
+        with the given exception message
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an inte")
+
+        if value <=0:
+            raise ValueError(f"{name} must be greater than 0")
