3717844b67011ee986904b119122365b8bd3bba9
Doctware <yusclever9@gmail.com>
2024-02-06 01:22:11 +0100

    3-main.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This function chekc if object is a kind 
+    kind of the specified class """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check base on the up documentation """
+    return isinstance(obj, a_class)
