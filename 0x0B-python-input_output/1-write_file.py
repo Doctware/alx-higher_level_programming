582aeb75e1e8832fbc958418d2261d5ba44cebc8
Doctware <yusclever9@gmail.com>
2024-02-08 00:32:27 +0100

    Just write it

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this function write a string to a text file (utf-8)
+    and returns the number of character written """
+
+
+def write_file(file_name="", text=""):
+    """
+    write a string to a text file
+    """
+    with open("filename", 'w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
