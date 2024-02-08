a37afcdfab33d5997838be9f5306408befff0869
Doctware <yusclever9@gmail.com>
2024-02-08 02:12:30 +0100

    Diak abd relAOD

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A class studet """
+
+
+class Student:
+    """
+    Represents a student with attributes first_name, last_name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object with the provided first name, last name, and age.
+
+        Parameters:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Parameters:
+            attrs (list of str, optional): A list of attribute names to retrieve.
+                If provided, only attributes with names in this list will be included in the dictionary.
+                If not provided or None, all attributes will be retrieved.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            attrs = self.__dict__.keys()
+
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
