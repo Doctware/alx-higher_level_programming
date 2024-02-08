b7c5f6991c1c66e2c3f9b32fd1ff829bf9a6a2af
Doctware <yusclever9@gmail.com>
2024-02-08 01:04:51 +0100

    JSON obj string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+""" A function that returns the JSON representation of
+    an object (string) """
+
+
+def to_json_string(my_obj):
+    """
+    return the JSON obj str
+    """
+    return json.dumps(my_obj)
