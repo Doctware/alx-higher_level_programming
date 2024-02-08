69efb4e74f3ebae2b637bfb568f45d9db7335c05
Doctware <yusclever9@gmail.com>
2024-02-08 01:22:37 +0100

    save to json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" importing JSON """
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Save a Python object to a text file using JSON representation.
+
+    Parameters:
+        my_obj (object): Python object to be saved.
+        filename (str): Name of the file to save the object to.
+
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
