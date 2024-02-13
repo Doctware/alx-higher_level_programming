ba73e44b9161f868faaaccf95e09572cff6ff3f4
Doctware <yusclever9@gmail.com>
2024-02-13 10:54:33 +0100

    Adding item to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" Module that adds args to JSON file | importing modules """
-
 import sys
-import os
 
-arg_list = sys.argv[1:]
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_JSON = __import__('5-save_to_json_file').save_to_json_file
-load_JSON = __import__('6-load_from_json_file').load_from_json_file
 
-lisst = []
-if os.path.exists('add_item.json'):
-    lisst = load_JSON('add_item.json')
+""" this script add items to a list """
 
-save_JSON(lisst + arg_list, "add_item.json")
+
+def add_items_to_list(filename, *args):
+    """
+    function:
+
+    add the given item to a list
+    """
+    items = load_from_json_file(filename)
+    items.extend(args)
+
+    save_to_json_file(args, filename)
+
+
+arguments = sys.argv[1:]
+add_items_to_list("add_item.json", *arguments)
