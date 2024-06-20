6afdf6ef509980d467b1d435af5ab4588ea00c4c
Doctware <yusclever9@gmail.com>
2024-06-20 03:24:04 +0100

    print my satement

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myStatement = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myStatement.lenght; i++) {
+for (let i = 0; i < myStatement.length; i++) {
   console.log(myStatement[i]);
 }
