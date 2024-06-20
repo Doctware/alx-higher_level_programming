e9fd6e4befcc3f7099f1b861667dfcac44e361aa
Doctware <yusclever9@gmail.com>
2024-06-20 02:59:21 +0100

    Thats my number :)

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const fArg = args[0];
-const number = parseInt(fArg, 10);
+const number = Math.floor(Number(fArg));
 
 if (isNaN(number)) {
   console.log('Not a number');
