18c97984ff610e941524ed26b2b9d7ef3717d9c8
Doctware <yusclever9@gmail.com>
2024-06-22 02:25:00 +0100

    check if it is postive and int

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  constructo (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+}
+
+module.exports = Rectangle;
