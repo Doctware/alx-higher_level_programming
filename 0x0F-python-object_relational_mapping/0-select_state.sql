5274fb71ec78eaa7c7a818641bedb54025d688e1
Doctware <yusclever9@gmail.com>
2024-06-26 09:47:46 +0100

    my sql script

---
+++ 0x0F-python-object_relational_mapping/0-select_state.sql
@@ -0,0 +1,13 @@
+-- create table im hbtn_0e_0usa with some data
+
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+
+USE hbtn_0e_0_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO states (name) VALUES ("California"), ("Arizonal"), ("Texas"), ("New York"), ("Nevada");
