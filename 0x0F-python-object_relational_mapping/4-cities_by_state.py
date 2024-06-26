91a13eee74dd821f9253a3b0a739f1a1d34a78c3
Doctware <yusclever9@gmail.com>
2024-06-26 20:17:12 +0100

    getting cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" this module contains a function thats list all cities in the
+    given database """
+import MySQLdb
+import sys
+
+
+def cities_by_state(username, password, database):
+    """ this function connect to the given database
+        the print cities by name """
+
+    db = MySQLdb.connec(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+    )
+
+    # creating cursor OBJ
+    cur = db.cursor()
+
+    # executing SQL query
+    query = """
+    SELECT cities.id, cities.name, statesi.name
+    FROM cities
+    JOIN states ON cities.states_id = states.id
+    ORDER BY cities.id ASC
+    """
+    cur.execute(query)
+
+    # getting the feched stuff
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__name__":
+    username, password, database = sys.argv[1:]
+
+    cities_by_state(username, password, database)
