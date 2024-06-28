2254a10091ff6503f7685a0b971012b55b89484f
Doctware <yusclever9@gmail.com>
2024-06-28 03:08:00 +0100

    adding new state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" this module contains instruction thats delete the states obj
-    which contains 'a' from the given DB """
+""" this module contains instruction thats changes the given states obj
+    from the given DB """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +8,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    username, password, database = sys.argv[1:]
+    username, password, database, = sys.argv[1:]
 
     # Connecting to mysql
     url = f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
@@ -21,13 +21,12 @@ if __name__ == "__main__":
     session = Session()
 
     # querying all state obj
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    state = session.query(State).filter_by(id=2).first()
 
-    # deleting
-    for state in states:
-        session.delete(state)
-
-    session.commit()
+    # printing resulr
+    if state:
+        state.name = "New Mexico"
+        session.commit()
 
     # terminate sesion
     session.close()
