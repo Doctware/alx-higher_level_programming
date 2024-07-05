807ecf30b60c9d4af4705cd5352d446675283019
Doctware <yusclever9@gmail.com>
2024-07-05 03:51:16 +0100

    cURL post params

--- 0x10-python-network_0/5-post-params.sh
+++ 0x10-python-network_0/5-post-params.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # take in a URL sends a post rquest to the passed url then display it body
+email="test@gmail.com"
+subject="I will always be here for PLD"
+
 curl -s -X POST -d "email=$email" -d "subject=$subject" "$1"
