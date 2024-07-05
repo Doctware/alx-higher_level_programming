480183fc8341f7c0081746a346ebaeac7a272f42
Doctware <yusclever9@gmail.com>
2024-07-05 02:58:18 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# take in a URL send a get request to the url and display
-# the body of the response
+# take in a URL send a get request to the url and display it body
 curl -sfL "$1" -X GET
