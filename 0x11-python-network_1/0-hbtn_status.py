f049f1f5ee83158564dacbc9e28ed39816dff780
Doctware <yusclever9@gmail.com>
2024-07-05 23:42:32 +0100

    checking the status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as res:
         result = res.read()
         print("Body response:")
         print(f'\t- type: {type(result)}')
