013488419adfb2e1102a6b8e9de7ccea35abc6f0
Doctware <yusclever9@gmail.com>
2024-07-05 23:38:15 +0100

    checking the status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ this module contains a code that feches the given URL """
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-    result = res.read()
-
 
 if __name__ == "__main__":
-    print("Body response:")
-    print(f'\t- type: {type(result)}')
-    print(f'\t- content: {result}')
-    print(f'\t- utf-8 content: {result.decode("utf-8")}')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        result = res.read()
+        print("Body response:")
+        print(f'\t- type: {type(result)}')
+        print(f'\t- content: {result}')
+        print(f'\t- utf-8 content: {result.decode("utf-8")}')
