6fccce9ef815d27b293acdd98d11785f129974bb
Doctware <yusclever9@gmail.com>
2024-07-10 01:51:21 +0100

    My GitHub id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,9 +3,9 @@
 Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id
 """
-
 import requests
 import sys
+
 
 def fetch_github_user_id(username, token):
     url = "https://api.github.com/user"
@@ -13,9 +13,10 @@ def fetch_github_user_id(username, token):
 
     if response.status_code == 200:
         user_data = response.json()
-        print(f"Your GitHub user ID is: {user_data.get('id')}")
+        print(user_data.get('id'))
     else:
-        print(f"Failed to fetch user ID. Status code: {response.status_code}")
+        print(response.status_code)
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
