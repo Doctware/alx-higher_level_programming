ec6b386659a313aee1434ace6fb56a7b90a37de5
Doctware <yusclever9@gmail.com>
2024-07-10 01:26:04 +0100

    search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,4 +8,24 @@ import requests
 import sys
 
 
-def send_post_request(url, letter)
+def search_letter(letter):
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': letter}
+    res = requests.post(url, data=data)
+
+    try:
+        json_res = res.json()
+        if json_res:
+            print(f"[{json_res.get('id')}] {json_res.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("not a valid json")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+        search_letter(letter)
